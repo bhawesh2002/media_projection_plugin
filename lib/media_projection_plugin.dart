66bb8771b3fdd403db8c19f4c64e45aa2e2ab27b
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-10-25 14:08:51 +0530

    feat: call startScreenCapture

--- lib/media_projection_plugin.dart
+++ lib/media_projection_plugin.dart
@@ -2,7 +2,7 @@
 import 'media_projection_plugin_platform_interface.dart';
 
 class MediaProjectionPlugin {
-  Future<String?> getPlatformVersion() {
-    return MediaProjectionPluginPlatform.instance.getPlatformVersion();
+  Future<bool?> startScreenCapture() {
+    return MediaProjectionPluginPlatform.instance.startScreenCapture();
   }
 }
