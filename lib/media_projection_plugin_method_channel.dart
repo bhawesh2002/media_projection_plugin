9ac382a46c81cd5a0a3b88f09423a02ac56a84cd
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-10-25 14:08:41 +0530

    feat: add implementation for startScreenCapture

--- lib/media_projection_plugin_method_channel.dart
+++ lib/media_projection_plugin_method_channel.dart
@@ -7,11 +7,11 @@ import 'media_projection_plugin_platform_interface.dart';
 class MethodChannelMediaProjectionPlugin extends MediaProjectionPluginPlatform {
   /// The method channel used to interact with the native platform.
   @visibleForTesting
-  final methodChannel = const MethodChannel('media_projection_plugin');
+  final methodChannel = const MethodChannel('com.media_projection_plugin/video');
 
   @override
-  Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
-    return version;
+  Future<bool?> startScreenCapture() async {
+    final result = await methodChannel.invokeMethod<String>('startScreenCapture');
+     return result as bool;
   }
 }
