7a2365087800fb333c088d616d4c0992f665b9c4
Bhavesh Mankar <bhaweshmankar2002@gmail.com>
2025-10-27 20:51:48 +0530

    feat: serialize MediaProjectionRequest, AudioProjectionPross and VideoProjectionProps classes

--- android/src/main/kotlin/com/example/media_projection_plugin/MediaProjectionRequest.kt
+++ android/src/main/kotlin/com/example/media_projection_plugin/MediaProjectionRequest.kt
@@ -1,39 +1,61 @@
 package com.example.media_projection_plugin
 
 import android.media.MediaRecorder
-import android.media.projection.MediaProjection
+import kotlinx.serialization.InternalSerializationApi
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
-/**
- * @param audioOnly If `true`  MediaProjection will capture audio only. Enabling will ignore the `videoRecordingProps`.
- */
-class MediaProjectionRequest(
+@OptIn(InternalSerializationApi::class)
+@Serializable
+data class MediaProjectionRequest(
     val audioOnly: Boolean = false,
     val videoRecordingProps: VideoRecordingProps = VideoRecordingProps(),
     val audioRecordingProps: AudioRecordingProps = AudioRecordingProps(),
-    var outputPath: String?,
-    val fileName: String?,
-    val exitCallback: MediaProjection.Callback?,
-    val maxDurationMs: Int?,
-    val maxFileSizeBytes: Long?,
-)
-
-
-class VideoRecordingProps(
+    val outputPath: String? = null,
+    val fileName: String? = null,
+    val maxDurationMs: Int? = null,
+    val maxFileSizeBytes: Long? = null,
+) {
+    companion object {
+        fun fromJson(json: String): MediaProjectionRequest {
+            return Json.decodeFromString(json)
+        }
+    }
+}
+@OptIn(InternalSerializationApi::class)
+@Serializable
+data class VideoRecordingProps(
     val videoSource: Int = MediaRecorder.VideoSource.SURFACE,
     val videoEncoder: Int = MediaRecorder.VideoEncoder.DEFAULT,
-    var screenWidth: Int? = null,
-    var screenHeight: Int? = null,
+    val screenWidth: Int? = null,
+    val screenHeight: Int? = null,
     val videoOpFormat: Int = MediaRecorder.OutputFormat.MPEG_4,
     val dpi: Int = 320,
     val videoBitrate: Int = 6 * 1024 * 1024, // 6 Mbps
     val fps: Int = 30,
-)
-
-class AudioRecordingProps(
+){
+    companion object {
+        @OptIn(InternalSerializationApi::class)
+        fun fromJson(json: String): VideoRecordingProps {
+            return Json.decodeFromString(json)
+        }
+    }
+}
+@OptIn(InternalSerializationApi::class)
+@Serializable
+data class AudioRecordingProps(
     val audioSource: Int = MediaRecorder.AudioSource.DEFAULT,
     val audioEncoder: Int = MediaRecorder.AudioEncoder.DEFAULT,
     val audioBitrate: Int = 128 * 1024, // 128 kbps
     val audioOpFormat: Int = MediaRecorder.OutputFormat.AAC_ADTS,
     val audioChannels: Int = 1,
     val audioSamplingRate: Int = 44100, // 44.1 kHz
-)
+){
+    companion object {
+        @OptIn(InternalSerializationApi::class)
+        fun fromJson(json: String): AudioRecordingProps {
+            return Json.decodeFromString(json)
+        }
+    }
+
+}
